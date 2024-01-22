150c7c8d958f5fec5a54f7a7aa0a7db34676cd59
Sachith Janjirala <sachith.janjirala@gmail.com>
2024-01-21 19:20:54 -0600

    refactored code; added modes

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ import requests
 
 st.set_page_config(layout='wide')
 
+st.title('Image 2 ASCII')
+st.subheader('''
+    :rainbow[An app that turns any Image to ASCII styled Image or Text.]''')
+
 with st.form('image_upload'):
-    image_file = st.file_uploader('Upload Image')
-    'OR'
-    image_file_url = st.text_input('Enter Image URL')
-    st.form_submit_button('Process Image')
+    c1, c2 = st.columns([0.7, 0.3], gap='large')
+    with c1:
+        image_file = st.file_uploader('Upload Image')
+        st.markdown('<div style="text-align: center"> OR </div>', unsafe_allow_html=True)
+        image_file_url = st.text_input('Enter Image URL')
+    with c2:
+        options = list(img2ascii.AVAILABLE_IMAGE_MODES.keys()) + list(img2ascii.AVAILABLE_TEXT_MODES.keys())
+        selected_mode = st.radio('Select Mode', options=options)
+        process_img = st.form_submit_button('Process Image', use_container_width=True)
 
 input_area = st.container()
 output_area = st.container()
@@ -21,9 +30,13 @@ elif image_file_url:
     stream = requests.get(image_file_url, stream=True).raw
     img = Image.open(stream)
 
-if img:
-    input_area.subheader('Original Image')
-    input_area.image(img)
+if process_img and img:
+    with st.spinner('Processing Image...'):
+        input_area.subheader('Original Image')
+        input_area.image(img)
 
-    output_area.subheader('ASCII version')
-    output_area.image(img2ascii.img2ascii_image(img))
+        output_area.subheader('ASCII version')
+        if selected_mode in img2ascii.AVAILABLE_IMAGE_MODES:
+            output_area.image(img2ascii.gen_ascii_image(img, mode=selected_mode))
+        else:
+            output_area.write('Development In Progress... Please Try Later!')
--- img2ascii.py
+++ img2ascii.py
@@ -4,36 +4,48 @@ import os
 
 register_heif_opener()
 
-CACH_PATH = 'cache'
+PIXEL_FILL_CHARS_SORTED_INC = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1\{\}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+CHAR_WIDTH, CHAR_HEIGHT = 10, 18 #based on observed font's letter width and height
+CHAR_ASPECT_RATIO = CHAR_WIDTH / CHAR_HEIGHT
+AVAILABLE_IMAGE_MODES = {
+    'RGB Image': 'RGB',
+    'Grayscale Image': 'L',
+    'Black / White Image': '1',
+    '8-bit Image': 'P'
+}
+AVAILABLE_TEXT_MODES = {
+    'ASCII TEXT': 'ascii_text'
+}
 
 def get_ascii_char(px_val, fill_vals=''):
-    PIXEL_FILL_CHARS_SORTED_INC = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1\{\}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
     if not fill_vals:
         fill_vals = PIXEL_FILL_CHARS_SORTED_INC
     ind = int((px_val / 256) * len(fill_vals))
     ascii_char = fill_vals[ind]
     return ascii_char
 
-def img2ascii_image(img, format='RGB'):
-    # img = Image.open(img_file)
-    CHAR_WIDTH, CHAR_HEIGHT = 10, 18 #based on observed font's letter width and height
-    CHAR_ASPECT_RATIO = CHAR_WIDTH / CHAR_HEIGHT
+def pre_process(img):
     scale_factor = 0.09
-
     img = img.resize(
         (int(img.width * scale_factor), int(img.height * scale_factor * CHAR_ASPECT_RATIO))
     )
-    img_width, img_height = img.size
+    return img
 
+def gen_ascii_image(img, mode='RGB'):
+    img = pre_process(img)
     px = img.load()
-
-    output_image = Image.new('RGB', (CHAR_WIDTH * img_width, CHAR_HEIGHT * img_height), color=(0, 0, 0))
-    output_image_draw = ImageDraw.Draw(output_image)
+    result_img = Image.new('RGB', (CHAR_WIDTH * img.width, CHAR_HEIGHT * img.height), color=(0, 0, 0))
+    result_img_draw = ImageDraw.Draw(result_img)
     font = ImageFont.truetype('fonts/lucon.ttf', 15)
 
-    for h in range(img_height):
-        for w in range(img_width):
+    for h in range(img.height):
+        for w in range(img.width):
             r, g, b = px[w, h]
             ascii_char = get_ascii_char(int(sum((r, g, b))/3))
-            output_image_draw.text((w*CHAR_WIDTH, h*CHAR_HEIGHT), ascii_char, font=font, fill=(r, g, b))
-    return output_image
+            result_img_draw.text((w*CHAR_WIDTH, h*CHAR_HEIGHT), ascii_char, font=font, fill=(r, g, b))
+    
+    result_img = result_img.convert(AVAILABLE_IMAGE_MODES[mode])
+    return result_img
+
+def gen_ascii_text(img, mode='ascii_text'):
+    pass
