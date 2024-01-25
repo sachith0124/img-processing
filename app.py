9e5dd7e45787d9fc6802eb0fe9c73ee13e388d8f
Sachith Janjirala <sachith.janjirala@gmail.com>
2024-01-25 16:39:54 -0600

    added sample images

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import img2ascii
 from PIL import Image
 import requests
-
-st.set_page_config(layout='wide')
+import util
+from st_click_detector import click_detector
 
 st.title('Image 2 ASCII')
 
@@ -15,11 +15,25 @@ with cols[1]:
     st.link_button('Go to Dev\'s Website ↗️', url='https://sachith.streamlit.app/')
 
 with st.form('image_upload'):
-    c1, c2 = st.columns([0.7, 0.3], gap='large')
+    c1, c2 = st.columns([0.8, 0.2], gap='small')
     with c1:
         image_file = st.file_uploader('Upload Image')
         st.markdown('<div style="text-align: center"> OR </div>', unsafe_allow_html=True)
         image_file_url = st.text_input('Enter Image URL')
+        st.markdown('<div style="text-align: center"> OR </div>', unsafe_allow_html=True)
+        with st.expander('Select from Sample Images', expanded=False):
+            st.caption('Images may be subject to copyright. I do not own these images. \n Images are being dynamically loaded from an external source.')
+            st.write("Click on image and click on 'Process Image' button!")
+            image_file_url = click_detector(util.load_imgs_html())
+            css='''
+            <style>
+                [data-testid="stExpander"] {
+                    overflow: scroll;
+                    height: 450px;
+                }
+            </style>
+            '''
+            st.markdown(css, unsafe_allow_html=True)
     with c2:
         options = list(img2ascii.AVAILABLE_IMAGE_MODES.keys()) + list(img2ascii.AVAILABLE_TEXT_MODES.keys())
         selected_mode = st.radio('Select Mode', options=options)
@@ -36,6 +50,7 @@ elif image_file_url:
     img = Image.open(stream)
 
 if process_img and img:
+    output_area.empty()
     with st.spinner('Processing Image...'):
         input_area.subheader('Original Image')
         input_area.image(img)
---
+++ util.py
@@ -0,0 +1,16 @@
+import requests
+from PIL import Image
+
+SAMPLE_IMAGE_URLS = [
+    "https://wallpapers-clan.com/wp-content/uploads/2023/11/marvel-iron-man-in-destroyed-suit-desktop-wallpaper-preview.jpg", 
+    "https://images.hdqwalls.com/wallpapers/iron-man-abstract-4k-b6.jpg"
+]
+
+def load_imgs_html():
+    # get_uri = lambda url: url.split('//')[1]
+    html_img = lambda img_url: f"<a href='#' id='{img_url}'><img width='70%' src='{img_url}'></a>"
+    html_content = ""
+    count = 1
+    for img_url in SAMPLE_IMAGE_URLS:
+        html_content += html_img(img_url) + "<br/>\n"*3
+    return html_content
